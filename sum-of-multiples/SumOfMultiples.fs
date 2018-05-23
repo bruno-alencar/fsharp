a05991bf332a0118620c350a91e4898491ec49ed
Bruno Alencar <bruno.idealencar@gmail.com>
2018-05-23 11:29:46 -0300

    Solve some tests and refactor code

--- sum-of-multiples/SumOfMultiples.fs
+++ sum-of-multiples/SumOfMultiples.fs
@@ -1,7 +1,9 @@
 ﻿module SumOfMultiples
 
 let sum (numbers: int list) (upperBound: int): int = 
-    List.sum (fun f -> upperBound % f = 0 )
+    numbers
+    |> List.filter (fun f -> upperBound % f = 0)
+    |> List.sum
 
 //     module SumOfMultiples
 
