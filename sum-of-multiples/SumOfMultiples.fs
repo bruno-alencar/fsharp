f69ade4ea4850227d45472b7b45181597e534f6f
Bruno Alencar <bruno.idealencar@gmail.com>
2018-05-25 13:17:31 -0300

    Solve some tests

--- sum-of-multiples/SumOfMultiples.fs
+++ sum-of-multiples/SumOfMultiples.fs
@@ -1,16 +1,15 @@
 ﻿module SumOfMultiples
 
-let sum (numbers: int list) (upperBound: int): int = 
-    numbers
-    |> List.filter (fun f -> upperBound % f = 0)
-    |> List.filter (fun f -> f = upperBound)
-    |> List.sum
-
-// let divides (input: int) (modulus: int): bool = input % modulus = 0
-
-// let isDivisibleByAny (numbers: int list) (input: int): bool = List.exists (divides input) numbers
-
 // let sum (numbers: int list) (upperBound: int): int = 
-//     [1..upperBound-1]
-//     |> List.filter (isDivisibleByAny numbers) 
-//     |> List.sum 
+//     [0..upperBound]
+//     |> List.filter (fun f -> upperBound % f = 0 && f = upperBound)
+//     |> List.sum
+
+let divides (input: int) (modulus: int): bool = input % modulus = 0
+
+let isDivisibleByAny (numbers: int list) (input: int): bool = List.exists (divides input) numbers
+
+let sum (numbers: int list) (upperBound: int): int = 
+    [0..upperBound]
+    |> List.filter (isDivisibleByAny numbers) 
+    |> List.sum 
