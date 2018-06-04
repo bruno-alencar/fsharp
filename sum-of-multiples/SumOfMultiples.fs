e58dad2192738f2a5960c3ebcad197cc80b57739
Bruno Alencar <bruno.idealencar@gmail.com>
2018-06-04 14:41:48 -0300

    Refactor code

--- sum-of-multiples/SumOfMultiples.fs
+++ sum-of-multiples/SumOfMultiples.fs
@@ -1,6 +1,6 @@
 ﻿module SumOfMultiples
 
-let change (numbers: int list) (value: int): int list = 
+let change (numbers: int list) (value: int): int = 
             numbers
             |> List.filter(fun f -> f % value = 0)
             // for item in number do
@@ -10,13 +10,11 @@ let sum (numbers: int list) (upperBound: int): int =
     [0..upperBound]
     |> change (numbers)
     |> List.sum
-    // |> List.sum
     // |> List.filter (fun f -> upperBound % f = 0 && f = upperBound)
 
 // let divides (input: int) (modulus: int): bool = input % modulus = 0
 
 // let isDivisibleByAny (numbers: int list) (input: int): bool = List.exists (divides input) numbers
-
 // let sum (numbers: int list) (upperBound: int): int = 
 //     [0..upperBound]
 //     |> List.filter (isDivisibleByAny numbers) 
