dbfe4b19250ee03ab102e0a25d916cac318c0b4c
Bruno Alencar <bruno.idealencar@gmail.com>
2018-06-11 00:51:02 -0300

    Clean code

--- sum-of-multiples/SumOfMultiples.fs
+++ sum-of-multiples/SumOfMultiples.fs
@@ -10,13 +10,3 @@
                     numbers 
                     |> List.exists(fun f -> e % f = 0))
         |> List.sum
-
-    // |> list.filter (fun f -> upperBound % f = 0 && f = upperBound)
-
-// let divides (input: int) (modulus: int): bool = input % modulus = 0
-
-// let isDivisibleByAny (numbers: int list) (input: int): bool = List.exists (divides input) numbers
-// let sum (numbers: int list) (upperBound: int): int = 
-//     [0..upperBound]
-//     |> List.filter (isDivisibleByAny numbers) 
-//     |> List.sum 
