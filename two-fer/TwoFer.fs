6070cc272df6ded5d46be730c7d936f542537e99
Bruno Alencar <bruno.idealencar@gmail.com>
2018-06-12 20:15:39 -0300

    Solve two fer exercise

--- two-fer/TwoFer.fs
+++ two-fer/TwoFer.fs
@@ -1,3 +1,8 @@
 ﻿module TwoFer
 
-let twoFer (input: string option): string = failwith "You need to implement this function."
+    let parameters (first: string) = "One for "+first+", one for me." 
+
+    let twoFer (input: string option): string =
+        match input with
+        | None -> parameters "you"
+        | _ -> parameters input.Value
