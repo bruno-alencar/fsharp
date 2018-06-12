bf0ec89f9f5a393803ddd92718425804b93506f3
Bruno Alencar <bruno.idealencar@gmail.com>
2018-06-12 20:34:21 -0300

    Start to create planets on space age exercise

--- space-age/SpaceAge.fs
+++ space-age/SpaceAge.fs
@@ -1,5 +1,11 @@
 ﻿module SpaceAge
 
 // TODO: define the Planet type
+type Planet() = 
+    member this.seconds = 2
 
-let age (planet: Planet) (seconds: int64): float = failwith "You need to implement this function."
+type Earth() =
+    member this.seconds = 31557600
+    member this.period = 365.25
+
+let age (planet: Planet) (seconds: int64): float = failwith "You need to implement this function."failwith "You need to implement this function."
