8ecf61e968cdc25cc715244c926d1032f708b8c6
Bruno Alencar <bruno.idealencar@gmail.com>
2018-06-12 20:35:40 -0300

    Add more planets on space age exercise

--- space-age/SpaceAge.fs
+++ space-age/SpaceAge.fs
@@ -1,11 +1,17 @@
 ﻿module SpaceAge
 
 // TODO: define the Planet type
-type Planet() = 
+type Planet = 
     member this.seconds = 2
 
-type Earth() =
+type Earth =
     member this.seconds = 31557600
     member this.period = 365.25
+
+type Mercury = 
+    member this.period = 0.2408467
+
+type Venus = 
+    member this.period = 0.61519726
 
 let age (planet: Planet) (seconds: int64): float = failwith "You need to implement this function."failwith "You need to implement this function."
