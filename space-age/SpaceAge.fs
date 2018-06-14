e8539813702efaefdaf76c5c589d4d166e8b9807
Bruno Alencar <bruno.idealencar@gmail.com>
2018-06-14 13:34:05 -0300

    Add inherit on objects

--- space-age/SpaceAge.fs
+++ space-age/SpaceAge.fs
@@ -9,22 +9,28 @@ type Earth =
     member this.period = 365.25
 
 type Mercury = 
+    inherit Planet 
     member this.period = 0.2408467
 
 type Venus = 
+    inherit Planet 
     member this.period = 0.61519726
 
 
 type Jupiter =
+    inherit Planet 
     member this.period = 11.862615
 
 type Saturn =
+    inherit Planet 
     member this.period = 29.447498
 
 type Uranus =
+    inherit Planet 
     member this.period = 84.016846
 
 type Neptune =
+    inherit Planet 
     member this.period = 164.79132
 
 let age (planet: Planet) (seconds: int64): float = failwith "You need to implement this function."failwith "You need to implement this function."
