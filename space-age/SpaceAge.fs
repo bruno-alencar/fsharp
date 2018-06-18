25fce9d2c50e3188d7e629db7a850e87d613e6ea
Bruno Alencar <bruno.idealencar@gmail.com>
2018-06-17 23:29:18 -0300

    Clean code

--- space-age/SpaceAge.fs
+++ space-age/SpaceAge.fs
@@ -16,46 +16,5 @@ type Planet =
     | Uranus
     | Neptune
     member this.seconds = 31557600
-    
-
-// type Planet = 
-//     new () = {}
-//     member this.seconds = 2
-
-// type Earth =
-//     inherit Planet 
-//     new () = {}
-//     member this.period = 365.25
-
-// type Mercury = 
-//     inherit Planet 
-//     new () = {}
-//     member this.period = 0.2408467
-
-// type Venus = 
-//     inherit Planet 
-//     new () = {}
-//     member this.period = 0.61519726
-
-
-// type Jupiter =
-//     inherit Planet 
-//     new () = {}
-//     member this.period = 11.862615
-
-// type Saturn =
-//     inherit Planet 
-//     new () = {}
-//     member this.period = 29.447498
-
-// type Uranus =
-//     inherit Planet 
-//     new () = {}
-//     member this.period = 84.016846
-
-// type Neptune =
-//     inherit Planet 
-//     new () = {}
-//     member this.period = 164.79132
-
-let age (planet: Planet) (seconds: int64): float = 
+ let age (planet: Planet) (seconds: int64): float = 
+    math
