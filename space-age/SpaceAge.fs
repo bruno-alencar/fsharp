79b0b22d807e547131ccaddb8fe7ed7f82862583
Bruno Alencar <bruno.idealencar@gmail.com>
2018-06-23 11:28:07 -0300

    Replace function to reuse on space age exercise

--- space-age/SpaceAge.fs
+++ space-age/SpaceAge.fs
@@ -18,11 +18,13 @@ type Planet =
     | Neptune
     member this.earth = float 31557600
 
+let round (value: float): float = 
+    System.Math.Round(value, 2)
 
 let age (planet: Planet) (seconds: int64): float = 
     match planet with
-        | Mercury -> System.Math.Round((seconds |> float) / (Mercury.earth * 0.2408467), 2)
-        | Earth -> System.Math.Round( (seconds |> float) / Earth.earth , 2)
-
+        | Mercury -> (seconds |> float) / (Mercury.earth * 0.2408467)
+        | Earth ->  (seconds |> float) / Earth.earth
+    |> round
        
     
