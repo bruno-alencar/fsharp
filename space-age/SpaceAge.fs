e3084da84229b08c631ac96291c1dc9eca512151
Bruno Alencar <bruno.idealencar@gmail.com>
2018-06-24 21:49:34 -0300

    Start to refactor space age exercise

--- space-age/SpaceAge.fs
+++ space-age/SpaceAge.fs
@@ -8,14 +8,14 @@ open System.Runtime.CompilerServices
 // TODO: define the Planet type
 
 type Planet =
-    | Earth
-    | Mercury
-    | Venus
-    | Mars
-    | Jupiter
-    | Saturn
-    | Uranus
-    | Neptune
+    | Earth -> 31557600
+    | Mercury ->  0.2408467
+    | Venus -> 0.61519726
+    | Mars -> 1.8808158
+    | Jupiter -> 11.862615
+    | Saturn -> 29.447498
+    | Uranus -> 84.016846
+    | Neptune -> 164.79132
     member this.earth = float 31557600
 
 let round (value: float): float = 
