de6966be56f8857aa89a9f1119e4c0d261d6f925
Bruno Alencar <bruno.idealencar@gmail.com>
2018-06-25 13:30:49 -0300

    Solve one test on space age exercise

--- space-age/SpaceAge.fs
+++ space-age/SpaceAge.fs
@@ -8,21 +8,31 @@ open System.Runtime.CompilerServices
 // TODO: define the Planet type
 
 type Planet =
-    | Earth -> 31557600
-    | Mercury ->  0.2408467
-    | Venus -> 0.61519726
-    | Mars -> 1.8808158
-    | Jupiter -> 11.862615
-    | Saturn -> 29.447498
-    | Uranus -> 84.016846
-    | Neptune -> 164.79132
+    | Earth
+    | Mercury
+    | Venus
+    | Mars
+    | Jupiter
+    | Saturn
+    | Uranus
+    | Neptune
     member this.earth = float 31557600
+
+    // Earth -> 31557600
+    // | Mercury ->  0.2408467
+    // | Venus -> 0.61519726
+    // | Mars -> 1.8808158
+    // | Jupiter -> 11.862615
+    // | Saturn -> 29.447498
+    // | Uranus -> 84.016846
+    // | Neptune -> 164.79132
 
 let round (value: float): float = 
     System.Math.Round(value, 2)
 
 let age (planet: Planet) (seconds: int64): float = 
     match planet with
+        | Venus -> (seconds |> float) / (Mercury.earth * 0.61519726)
         | Mercury -> (seconds |> float) / (Mercury.earth * 0.2408467)
         | Earth ->  (seconds |> float) / Earth.earth
     |> round
