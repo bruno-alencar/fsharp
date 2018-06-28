646811388915b28c53b33aec021c66123a798c58
Bruno Alencar <bruno.idealencar@gmail.com>
2018-06-28 20:49:29 -0300

    Start to refactor code on raindrops exercise

--- raindrops/Raindrops.fs
+++ raindrops/Raindrops.fs
@@ -2,19 +2,20 @@
 open System.Globalization
 open System.Runtime.ExceptionServices
 
-let convert (number: int): string = 
-    let factor = 
-        [1..number]
-        |> List.filter(fun e -> number % e = 0)
-        |> List.map(fun e -> 
-            match e with
-                | 3 -> sprintf "Pling"
-                | 5 -> sprintf "Plang"
-                | 7 -> sprintf "Plong"
-                | _ -> null
-        )  
-        |> List.filter(fun g -> g <> null)
-    
+let concatenate (number: int) (factor :string list) : string = 
     match factor with
         | [] -> number.ToString()
         | xs -> String.concat "" xs
+
+let convert (number: int): string = 
+    [1..number]
+    |> List.filter(fun e -> number % e = 0)
+    |> List.map(fun e -> 
+        match e with
+            | 3 -> sprintf "Pling"
+            | 5 -> sprintf "Plang"
+            | 7 -> sprintf "Plong"
+            | _ -> null
+    )  
+    |> List.filter(fun g -> g <> null)
+    |> concatenate number
