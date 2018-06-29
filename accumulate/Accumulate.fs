4d11701557d4c12dc9e748dc08605d62d7ff054c
Bruno Alencar <bruno.idealencar@gmail.com>
2018-06-29 14:57:50 -0300

    Start to solve accumulate exercise

--- accumulate/Accumulate.fs
+++ accumulate/Accumulate.fs
@@ -1,3 +1,8 @@
 ﻿module Accumulate
 
-let accumulate<'a, 'b> (func: 'a -> 'b) (input: 'a list): 'b list = failwith "You need to implement this function."
+let accumulate<'a, 'b> (func: 'a -> 'b) (input: 'a list): 'b list =
+    input
+    |> yield func
+    |> List.toArray()
+    
+    
