d25b7d4897af6aec95c089bdafcd8574d8ac0dbf
Bruno Alencar <bruno.idealencar@gmail.com>
2018-07-03 08:47:26 -0300

    Remove unused code

--- grade-school/GradeSchool.fs
+++ grade-school/GradeSchool.fs
@@ -4,8 +4,8 @@ type School = Map<int, string list>
 
 let empty: School = string.empty
 
-let add (student: string) (grade: int) (school: School): School = failwith "You need to implement this function."
+let add (student: string) (grade: int) (school: School): School = 
 
-let roster (school: School): (int * string list) list = failwith "You need to implement this function."
+let roster (school: School): (int * string list) list = 
 
-let grade (number: int) (school: School): string list = failwith "You need to implement this function."
+let grade (number: int) (school: School): string list = 
