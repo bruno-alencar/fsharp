29b7d975a444e953b275341ec0f0089212954672
Bruno Alencar <bruno.idealencar@gmail.com>
2018-07-08 22:13:44 -0300

    Solve all tests on grade school exercise

--- grade-school/GradeSchool.fs
+++ grade-school/GradeSchool.fs
@@ -6,12 +6,12 @@ let empty: School = Map.empty
 
 let add (student: string) (grade: int) (school: School): School = 
     match school.TryFind grade with
-        | Some value -> school.Add (grade, (student :: value))
-        | None -> school.Add(grade, [student])
+        | Some value -> school.Add (grade, (student :: value |> List.sort))
+        | None -> school.Add (grade, [string student])
 
 let roster (school: School): (int * string list) list = school |> Map.toList
 
 let grade (number: int) (school: School): string list =  
     match school.TryFind number with
-        | Some value -> value
+        | Some value -> value 
         | None -> []
