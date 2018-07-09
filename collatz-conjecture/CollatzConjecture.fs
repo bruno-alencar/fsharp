a9bcfe1ac7bb93b27fb4143c1ff31472a49fe571
Bruno Alencar <bruno.idealencar@gmail.com>
2018-07-08 22:47:28 -0300

    Replace some items on collatz exercise

--- collatz-conjecture/CollatzConjecture.fs
+++ collatz-conjecture/CollatzConjecture.fs
@@ -2,5 +2,5 @@
 
 let steps (number: int): int option = 
     match number % 2 = 0 with
-    | Some -> steps  (number / 2)
-    | None -> steps (3 * number + 1)
+    | Some value -> steps  (value / 2)
+    | None -> steps (3 * value + 1)
