0a726dfa552492367dffe5febcea1bebd6416728
Bruno Alencar <bruno.idealencar@gmail.com>
2018-07-08 22:38:55 -0300

    Start to create solution to collatz exercise

--- collatz-conjecture/CollatzConjecture.fs
+++ collatz-conjecture/CollatzConjecture.fs
@@ -1,3 +1,6 @@
 ﻿module CollatzConjecture
 
-let steps (number: int): int option = failwith "You need to implement this function."
+let steps (number: int): int option = 
+    match number % 2 = 0 with
+    | Some -> steps  (number / 2)
+    | None -> steps (3 * number + 1)
