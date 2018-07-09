2b46e0b984ea429ffd853682739a4fc4a8cedeb3
Bruno Alencar <bruno.idealencar@gmail.com>
2018-07-09 17:05:13 -0300

    Solve one exercise on collatz conjecture

--- collatz-conjecture/CollatzConjecture.fs
+++ collatz-conjecture/CollatzConjecture.fs
@@ -1,6 +1,9 @@
 ﻿module CollatzConjecture
 
 let steps (number: int): int option = 
-    match number % 2 = 0 with
-    | Some value -> steps  (value / 2)
-    | None -> steps (3 * value + 1)
+    match number with
+    | _ when number % 2 = 0 -> Some number
+    | _ -> Some 0
+
+    // | _ when number < 0 -> steps  (value / 2)
+    // | None -> steps (3 * value + 1) 
