d1ada74b76cc8063fcdee400b924292edc914f87
Bruno Alencar <bruno.idealencar@gmail.com>
2018-07-15 12:40:12 -0300

    Refactor and clean code on collatz conjecture exercise

--- collatz-conjecture/CollatzConjecture.fs
+++ collatz-conjecture/CollatzConjecture.fs
@@ -1,34 +1,15 @@
 ﻿module CollatzConjecture
 
-    // let rec calculate (x: int): int option =
-    //     let counted f =
-    //           let count = ref 0
-    //           (fun calc -> 
-    //             if x <= 1 then
-    //                 Some count
-    //             else if  x % 2 = 0 then
-    //                 count := count + 1
-    //                 calc (x / 2)
-    //             else 
-    //                 calc (3 * x + 1))
-    //     Some (counted x)
-
-    let nextNumber number = 
-        if number % 2 = 0 then
-           number / 2
-        else 
-            number * 3 + 1
+    let rec calculate count number =
+            match number with
+            | number when number <= 0 -> None
+            | number when number = 1 -> Some count
+            | number -> 
+                match number % 2 = 0 with
+                    | true -> number / 2
+                    | false -> number * 3 + 1  
+                |> calculate (count + 1)
 
     let steps (number: int): int option = 
-        let rec calculate acc n =
-            match n with
-            | n when n <= 0 -> None
-            | n when n = 1 -> Some acc
-            | n -> 
-                if n % 2 = 0 then
-                   n / 2
-                else 
-                    n * 3 + 1  
-                |> calculate (acc + 1)
         calculate 0 number
              
