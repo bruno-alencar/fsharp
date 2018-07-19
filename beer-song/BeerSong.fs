f6e10ad596937f239a92354a159b9bfc9952313c
Bruno Alencar <bruno.idealencar@gmail.com>
2018-07-19 14:14:09 -0300

    Start to create solution for beer song

--- beer-song/BeerSong.fs
+++ beer-song/BeerSong.fs
@@ -1,3 +1,9 @@
 ﻿module BeerSong
 
-let recite (startBottles: int) (takeDown: int) = failwith "You need to implement this function."
+let bottles x:int = "%i bottles of beer on the wall, 99 bottles of beer." x;
+
+
+let recite (startBottles: int) (takeDown: int) = 
+    [startBottles..startBottles-takeDown]
+    |> "%i bottles of beer on the wall, 99 bottles of beer.";
+          "Take one down and pass it around, %i bottles of beer on the wall." startBottles (startBottles-1)
