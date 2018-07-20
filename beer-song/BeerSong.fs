fffe85c1d84a634cd09074230600fa2d8876a19e
Bruno Alencar <bruno.idealencar@gmail.com>
2018-07-19 23:47:49 -0300

    Replace methods to use new recursive on beer song exercise

--- beer-song/BeerSong.fs
+++ beer-song/BeerSong.fs
@@ -1,9 +1,19 @@
 ﻿module BeerSong
 
-let bottles x:int = "%i bottles of beer on the wall, 99 bottles of beer." x;
-
+// let bottles x = "%i bottles of beer on the wall, 99 bottles of beer." x ;
+let rec bottles number (list: string list)=
+    match number with
+        | 1 -> ["None bottles of beer on the wall, 99 bottles of beer.";
+        "Take one down and pass it around, 98 bottles of beer on the wall."]
+        | number when number > 0 -> 
+               ["%i bottles of beer on the wall, %i bottles of beer." number (number-1);
+                "Take one down and pass it around, %i bottles of beer on the wall." (number-1)]
+     |> list.concat
+     |> bottles (number-1)
+    //  bottles list number-1
+    
 
 let recite (startBottles: int) (takeDown: int) = 
     [startBottles..startBottles-takeDown]
-    |> "%i bottles of beer on the wall, 99 bottles of beer.";
-          "Take one down and pass it around, %i bottles of beer on the wall." startBottles (startBottles-1)
+    |> List.add "%i bottles of beer on the wall, 99 bottles of beer.";
+          "Take one down and pass it around, %i bottles of beer on the wall." startBottles (startBottles-1) 
