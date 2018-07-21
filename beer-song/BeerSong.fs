65368fd864ed3563a9437120d9bab825d7201991
Bruno Alencar <bruno.idealencar@gmail.com>
2018-07-20 23:33:36 -0300

    Solve one test on beer song exercise

--- beer-song/BeerSong.fs
+++ beer-song/BeerSong.fs
@@ -1,18 +1,16 @@
 ﻿module BeerSong
+open System.Globalization
 
-let rec bottles number (list: string list)=
+let rec bottles (number: int): string list =
     match number with
-        | 1 -> ["None bottles of beer on the wall, 99 bottles of beer.";
-        "Take one down and pass it around, 98 bottles of beer on the wall."]
-        | number when number > 0 -> 
-               ["%i bottles of beer on the wall, %i bottles of beer." number (number-1);
-                "Take one down and pass it around, %i bottles of beer on the wall." (number-1)]
-     |> list.concat
-     |> bottles (number-1)
-    //  bottles list number-1
-    
+        | 0 -> ["No more bottles of beer on the wall, 99 bottles of beer.";
+                "Go to the store and buy some more, 99 bottles of beer on the wall."]
+        | 1 -> [sprintf "%i bottles of beer on the wall, %i bottles of beer." number number;"Take it down and pass it around, no more bottles of beer on the wall."]
+        | _ -> [sprintf "%i bottles of beer on the wall, %i bottles of beer." number number; sprintf "Take one down and pass it around, %i bottles of beer on the wall." (number-1)]
 
 let recite (startBottles: int) (takeDown: int) = 
-    [startBottles..startBottles-takeDown]
-    |> List.add "%i bottles of beer on the wall, 99 bottles of beer.";
-          "Take one down and pass it around, %i bottles of beer on the wall." startBottles (startBottles-1) 
+    // [startBottles..takeDown]
+    // List.map 
+    bottles startBottles
+    
+    
