0403989cc01a00fa5f8892839815452ee4dd70ea
Bruno Alencar <bruno.idealencar@gmail.com>
2018-07-20 23:53:37 -0300

    refactor code to run other tests

--- beer-song/BeerSong.fs
+++ beer-song/BeerSong.fs
@@ -6,7 +6,7 @@ let rec bottles (number: int): string list =
         | 0 -> ["No more bottles of beer on the wall, 99 bottles of beer.";
                 "Go to the store and buy some more, 99 bottles of beer on the wall."]
         | 1 -> [sprintf "%i bottles of beer on the wall, %i bottles of beer." number number;"Take it down and pass it around, no more bottles of beer on the wall."]
-        | _ -> [sprintf "%i bottles of beer on the wall, %i bottles of beer." number number; sprintf "Take one down and pass it around, %i bottles of beer on the wall." (number-1)]
+        | _ -> [sprintf "%i bottles of beer on the wall, %i bottles of beer." number number;  sprintf "Take one down and pass it around, %i bottles of beer on the wall." (number-1)]
 
 let recite (startBottles: int) (takeDown: int) = 
     // [startBottles..takeDown]
