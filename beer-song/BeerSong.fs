0971ba041c64cb4ad7c8fb6a07b09f68838ed642
Bruno Alencar <bruno.idealencar@gmail.com>
2018-07-21 01:52:56 -0300

    Replace beer song to solve some tests

--- beer-song/BeerSong.fs
+++ beer-song/BeerSong.fs
@@ -3,13 +3,12 @@ open System.Globalization
 
 let rec bottles (number: int): string list =
     match number with
-        | 0 -> ["No more bottles of beer on the wall, 99 bottles of beer.";
-                "Go to the store and buy some more, 99 bottles of beer on the wall."]
+        | 0 -> ["No more bottles of beer on the wall, 99 bottles of beer."; "Go to the store and buy some more, 99 bottles of beer on the wall."]
         | 1 -> [sprintf "%i bottles of beer on the wall, %i bottles of beer." number number;"Take it down and pass it around, no more bottles of beer on the wall."]
-        | _ -> [sprintf "%i bottles of beer on the wall, %i bottles of beer." number number;  sprintf "Take one down and pass it around, %i bottles of beer on the wall." (number-1)]
+        | _ -> [sprintf "%i bottles of beer on the wall, %i bottles of beer." number number;  sprintf "\nTake one down and pass it around, %i bottles of beer on the wall." (number-1)]
 
 let recite (startBottles: int) (takeDown: int) = 
-    // [startBottles..takeDown]
+    // [startBottles..takeDown] 
     // List.map 
     bottles startBottles
     
