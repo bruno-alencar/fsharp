2e9183ffeee31485890cb9063e0f186f28a1c88d
Bruno Alencar <bruno.idealencar@gmail.com>
2018-07-20 23:34:43 -0300

    Solve some tests on beer song

--- beer-song/BeerSongTest.fs
+++ beer-song/BeerSongTest.fs
@@ -14,14 +14,14 @@ let ``First generic verse`` () =
           "Take one down and pass it around, 98 bottles of beer on the wall." ]
     recite 99 1 |> should equal expected
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Last generic verse`` () =
     let expected = 
         [ "3 bottles of beer on the wall, 3 bottles of beer.";
           "Take one down and pass it around, 2 bottles of beer on the wall." ]
     recite 3 1 |> should equal expected
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Verse with 2 bottles`` () =
     let expected = 
         [ "2 bottles of beer on the wall, 2 bottles of beer.";
