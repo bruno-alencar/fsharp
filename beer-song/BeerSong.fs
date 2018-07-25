fe6b3df5c37827b5b995f9b27df5dbfc47aaf7a9
Bruno Alencar <bruno.idealencar@gmail.com>
2018-07-25 13:58:06 -0300

    Solve some tests on beer song

--- beer-song/BeerSong.fs
+++ beer-song/BeerSong.fs
@@ -5,7 +5,10 @@ let rec bottles (number: int)=
     match number with
         | 0 -> ["No more bottles of beer on the wall, 99 bottles of beer."; "Go to the store and buy some more, 99 bottles of beer on the wall."]
         | 1 -> [sprintf "%i bottles of beer on the wall, %i bottles of beer." number number;"Take it down and pass it around, no more bottles of beer on the wall."]
-        | _ -> [sprintf "%i bottles of beer on the wall, %i bottles of beer." number number;  sprintf "\nTake one down and pass it around, %i bottles of beer on the wall." (number-1)]
+        | 2 -> [sprintf "%i bottles of beer on the wall, %i bottles of beer." number  number; sprintf "Take one down and pass it around, %i bottle of beer on the wall." (number-1)]
+        | _ -> [sprintf "%i bottles of beer on the wall, %i bottles of beer." number  number; sprintf "Take one down and pass it around, %i bottles of beer on the wall." (number-1)]
+
+        
         //  | _ -> sprintf "%i bottles of beer on the wall, %i bottles of beer.\nTake one down and pass it around, %i bottles of beer on the wall.\n"
         //     number
         //     number
@@ -14,5 +17,5 @@ let recite (startBottles: int) (takeDown: int) =
     // [startBottles..takeDown] 
     // List.map 
     bottles startBottles
-    
+        
     
