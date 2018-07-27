4146242a61821986de0d89a3034a59dc586c0421
Bruno Alencar <bruno.idealencar@gmail.com>
2018-07-27 00:22:06 -0300

    Refactor code on beer song

--- beer-song/BeerSong.fs
+++ beer-song/BeerSong.fs
@@ -1,23 +1,20 @@
 ﻿module BeerSong
 open System.Globalization
 
+let getend (number: int) = 
+    match number with
+        | 0 -> "Go to the store and buy some more, 99 bottles of beer on the wall."
+        | 1 -> "Take it down and pass it around, no more bottles of beer on the wall."
+        | 2 -> sprintf "Take one down and pass it around, %i bottle of beer on the wall." (number-1)
+        | _ -> sprintf "Take one down and pass it around, %i bottles of beer on the wall.\n" (number-1)
+
 let bottles (number: int)=
     match number with
-        | 0 -> "No more bottles of beer on the wall, no more bottles of beer.";"\n Go to the store and buy some more, 99 bottles of beer on the wall."
-        | 1 -> sprintf "%i bottle of beer on the wall, %i bottle of beer. \n Take it down and pass it around, no more bottles of beer on the wall." number number
-        | 2 -> sprintf "%i bottles of beer on the wall, %i bottles of beer. \n Take one down and pass it around, %i bottle of beer on the wall." number number (number-1)
-        | _ -> sprintf "%i bottles of beer on the wall, %i bottles of beer. \n Take one down and pass it around, %i bottles of beer on the wall." number number (number-1)
-        
-        //  | _ -> sprintf "%i bottles of beer on the wall, %i bottles of beer.\nTake one down and pass it around, %i bottles of beer on the wall.\n"
-        //     number
-        //     number
-        //     (number - 1)
+        | 0 -> ["No more bottles of beer on the wall, no more bottles of beer.";"Go to the store and buy some more, 99 bottles of beer on the wall."]
+        | 1 -> [sprintf "%i bottle of beer on the wall, %i bottle of beer."number number;"Take it down and pass it around, no more bottles of beer on the wall."]
+        | 2 -> [sprintf "%i bottles of beer on the wall, %i bottles of beer." number number; sprintf "Take one down and pass it around, %i bottle of beer on the wall." (number-1)]
+        | _ -> [sprintf "%i bottles of beer on the wall, %i bottles of beer."number number; sprintf "Take one down and pass it around, %i bottles of beer on the wall." (number-1)]
 
 let recite (startBottles: int) (takeDown: int) =
-    [startBottles.. -1 ..takeDown]
-    |> List.map bottles
-
-    // List.map bottles list
-    // |> List.append list
-        
-    
+    List.collect bottles [startBottles.. -1 ..startBottles-takeDown+1]
+    // |> List.concat "\n"
