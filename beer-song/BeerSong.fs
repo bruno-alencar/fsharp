b1cc2245b581998a2174c7399aa5e47733e56738
Bruno Alencar <bruno.idealencar@gmail.com>
2018-07-27 22:12:06 -0300

    Solve some errors on beer song

--- beer-song/BeerSong.fs
+++ beer-song/BeerSong.fs
@@ -4,9 +4,12 @@ open System.Globalization
 let bottles (number: int)=
     match number with
         | 0 -> ["No more bottles of beer on the wall, no more bottles of beer.";"Go to the store and buy some more, 99 bottles of beer on the wall."]
-        | 1 -> [sprintf "%i bottle of beer on the wall, %i bottle of beer."number number;"Take it down and pass it around, no more bottles of beer on the wall."]
-        | 2 -> [sprintf "%i bottles of beer on the wall, %i bottles of beer." number number; sprintf "Take one down and pass it around, %i bottle of beer on the wall." (number-1)]
-        | _ -> [sprintf "%i bottles of beer on the wall, %i bottles of beer."number number; sprintf "Take one down and pass it around, %i bottles of beer on the wall." (number-1)]
+        | 1 -> [sprintf "%i bottle of beer on the wall, %i bottle of beer."number number; 
+                "Take it down and pass it around, no more bottles of beer on the wall."]
+        | 2 -> [(sprintf "%i bottles of beer on the wall, %i bottles of beer." number number); 
+                (sprintf "Take one down and pass it around, %i bottle of beer on the wall." (number-1))]
+        | _ -> [ (sprintf "%i bottles of beer on the wall, %i bottles of beer."number number);
+                (sprintf "Take one down and pass it around, %i bottles of beer on the wall." (number-1));(sprintf "")]
 
 let recite (startBottles: int) (takeDown: int) =
     List.collect bottles [startBottles.. -1 ..startBottles-takeDown+1]
