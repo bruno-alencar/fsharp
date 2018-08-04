292bd20316574411784bc5398410270df41d75d7
Bruno Alencar <bruno.idealencar@gmail.com>
2018-08-04 01:05:30 -0300

    add modifications on beer song

--- beer-song/BeerSong.fs
+++ beer-song/BeerSong.fs
@@ -5,8 +5,8 @@ let bottles (number: int)=
     match number with
         | 0 -> ["No more bottles of beer on the wall, no more bottles of beer.";"Go to the store and buy some more, 99 bottles of beer on the wall."]
         | 1 -> [sprintf "%i bottle of beer on the wall, %i bottle of beer."number number;"Take it down and pass it around, no more bottles of beer on the wall."]
-        | 2 -> [sprintf "%i bottles of beer on the wall, %i bottles of beer." number number; sprintf "Take one down and pass it around, %i bottle of beer on the wall." (number-1)]
-        | _ -> [sprintf "%i bottles of beer on the wall, %i bottles of beer."number number; sprintf "Take one down and pass it around, %i bottles of beer on the wall." (number-1)]
+        | 2 -> [sprintf "%i bottles of beer on the wall, %i bottles of beer." number number; sprintf "Take one down and pass it around, %i bottle of beer on the wall." (number-1);]
+        | _ -> [sprintf "%i bottles of beer on the wall, %i bottles of beer."number number; sprintf "Take one down and pass it around, %i bottles of beer on the wall." (number-1);]
 
 let recite (startBottles: int) (takeDown: int) =
     List.collect bottles [startBottles.. -1 ..startBottles-takeDown+1]
