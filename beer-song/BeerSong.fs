bc18eba182ad17b5af58ed54a0c57e6967c76490
Bruno Alencar <bruno.idealencar@gmail.com>
2018-08-06 20:59:30 -0300

    replace method

--- beer-song/BeerSong.fs
+++ beer-song/BeerSong.fs
@@ -20,19 +20,22 @@ let bottles (number: int)=
         | 1 -> [sprintf "%i bottle of beer on the wall, %i bottle of beer."number number;"Take it down and pass it around, no more bottles of beer on the wall."]
         | 2 -> [sprintf "%i bottles of beer on the wall, %i bottles of beer." number number; sprintf "Take one down and pass it around, %i bottle of beer on the wall." (number-1);]
         | _ -> [sprintf "%i bottles of beer on the wall, %i bottles of beer."number number; sprintf "Take one down and pass it around, %i bottles of beer on the wall." (number-1);]
-        | 0 -> ["No more bottles of beer on the wall, no more bottles of beer.";
-                "Go to the store and buy some more, 99 bottles of beer on the wall."]
-        | 1 -> [sprintf "%i bottle of beer on the wall, %i bottle of beer."number number; 
-                "Take it down and pass it around, no more bottles of beer on the wall."]
-        | 2 -> [(sprintf "%i bottles of beer on the wall, %i bottles of beer." number number); 
-                (sprintf "Take one down and pass it around, %i bottle of beer on the wall." (number-1))]
-        | _ -> [(sprintf "%i bottles of beer on the wall, %i bottles of beer."number number);
-                (sprintf "Take one down and pass it around, %i bottles of beer on the wall." (number-1))] 
+        // | 0 -> ["No more bottles of beer on the wall, no more bottles of beer.";
+        //         "Go to the store and buy some more, 99 bottles of beer on the wall."]
+        // | 1 -> [sprintf "%i bottle of beer on the wall, %i bottle of beer."number number; 
+        //         "Take it down and pass it around, no more bottles of beer on the wall."]
+        // | 2 -> [(sprintf "%i bottles of beer on the wall, %i bottles of beer." number number); 
+        //         (sprintf "Take one down and pass it around, %i bottle of beer on the wall." (number-1))]
+        // | _ -> [(sprintf "%i bottles of beer on the wall, %i bottles of beer."number number);
+        //         (sprintf "Take one down and pass it around, %i bottles of beer on the wall." (number-1))] 
 
 let recite (startBottles: int) (takeDown: int) =
+        // List.collect bottles  ;[""];
+        List.collect bottles [startBottles.. -1 ..startBottles-takeDown+1]
+        |> List.concat [""]
+        // |> List.concat [bottles;[""];recite (startBottles - 1) (takeDown - 1)]
 //     List.collect bottles [startBottles.. -1 ..startBottles-takeDown+1]
-        String.concat "" [for i in startBottles .. -1 .. startBottles-takeDown+1 -> bottles i |> Array.append ["\n"]]
-
+        // String.concat "" [for i in startBottles .. -1 .. startBottles-takeDown+1 -> bottles i |> Array.append ["\n"]]
         // let list = []
         // [startBottles.. -1 ..startBottles-takeDown+1]
         // |> List.map bottles
