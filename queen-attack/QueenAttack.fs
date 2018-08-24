afa8bb781cbb71084e0f958b1bba2d503d17e804
Bruno Alencar <bruno.idealencar@gmail.com>
2018-08-24 20:46:33 -0300

    Add queen can attack method

--- queen-attack/QueenAttack.fs
+++ queen-attack/QueenAttack.fs
@@ -4,4 +4,7 @@ let create (position: int * int) =
     match position with
     | (a, b) -> (a > 0 && b > 0 ) && (a < 8 && b < 8)  
 
-let canAttack (queen1: int * int) (queen2: int * int) = false
+let canAttack (queen1: int * int) (queen2: int * int) = 
+    let q1 = queen1 |> create
+    let q2 = queen2 |> create
+    q1 && q2
