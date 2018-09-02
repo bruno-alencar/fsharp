698d0b349120d743c1a0f67ff75a5afc9b1d393f
Bruno Alencar <bruno.idealencar@gmail.com>
2018-09-01 23:01:13 -0300

    'Solve queen attack

--- queen-attack/QueenAttack.fs
+++ queen-attack/QueenAttack.fs
@@ -4,6 +4,7 @@ let create (position: int * int) =
     match position with
     | (a, b) -> (a > 0 && b > 0 ) && (a < 8 && b < 8)  
 
+
 let canAttack (queen1: int * int) (queen2: int * int) = 
     let (x1, y1) = queen1
     let (x2, y2) = queen2
