f8d02a9103de364478027ef381a8dd6a4a3b6a98
Bruno Alencar <bruno.idealencar@gmail.com>
2018-09-20 13:34:57 -0300

    Add enums

--- kindergarten-garden/KindergartenGarden.fs
+++ kindergarten-garden/KindergartenGarden.fs
@@ -2,13 +2,19 @@
 
 // TODO: define the Plant type
 type Plant = 
-    | Grass = 'G'
-    | Clover = 'C'
-    | Violets = 'V'
-    | Radishes = 'R'
+| Grass = 'G'
+| Clover = 'C'
+| Violets = 'V'
+| Radishes = 'R'
+
+let students = [
+    "Alice"; "Bob"; "Charlie"; "David"; "Eve"; "Fred"; 
+    "Ginny"; "Harriet"; "Ileana"; "Joseph"; "Kincaid"; "Larry"
+]
 
 let plants diagram student = 
-    match diagram with 
-    |  a as Plant  -> a
-    | _ -> ""
-
+    match plants with
+    | 'C' -> Clover
+    | 'R' -> Radishes
+    | 'G' -> Grass
+    | 'V' -> Violets
