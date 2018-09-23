3d715383238990412b7a6fe54619ebc4ba84ad44
Bruno Alencar <bruno.idealencar@gmail.com>
2018-09-23 01:52:08 -0300

    Add step on solution

--- kindergarten-garden/KindergartenGarden.fs
+++ kindergarten-garden/KindergartenGarden.fs
@@ -11,9 +11,20 @@ let students = [
     "Ginny"; "Harriet"; "Ileana"; "Joseph"; "Kincaid"; "Larry"
 ]
 
-let plants diagram student = 
-    // match plants with
-    // | 'C' -> Clover
-    // | 'R' -> Radishes
-    // | 'G' -> Grass
-    // | 'V' -> Violets
+// let plants diagram student = 
+//     // match plants with
+//     // | 'C' -> Clover
+//     // | 'R' -> Radishes
+//     // | 'G' -> Grass
+//     // | 'V' -> Violets
+//     "Alice"; "Bob"; "Charlie"; "David"; "Eve"; "Fred"; 
+//     "Ginny"; "Harriet"; "Ileana"; "Joseph"; "Kincaid"; "Larry"
+// ]
+
+let charToPlant = LanguagePrimitives.EnumOfValue
+let rowToPlants (idx:int) (row:string) = [charToPlant row.[idx]; charToPlant row.[idx + 1]]
+
+let plants (diagram: string) (student: string): Plant list = 
+    let studentIdx = (List.findIndex ((=) student) students) * 2
+    let rows = diagram.Split('\n') |> Array.toList
+    rows |> List.collect (fun row -> rowToPlants studentIdx row)
