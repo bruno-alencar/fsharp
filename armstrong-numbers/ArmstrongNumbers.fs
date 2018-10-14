ad5ddf179fd8dc20dbcf7a7d72c87e846e42218a
Bruno Alencar <bruno.idealencar@gmail.com>
2018-10-14 00:01:38 -0300

    Solve armstrong

--- armstrong-numbers/ArmstrongNumbers.fs
+++ armstrong-numbers/ArmstrongNumbers.fs
@@ -2,5 +2,5 @@
 
 let isArmstrongNumber (number: int): bool = 
     let chars = [for c in string number -> float (c.ToString())]
-    // let length = float chars.Length
-    // chars |> List.sumBy (fun x -> int (x ** length)) |> (=) number
+    let length = float chars.Length
+    chars |> List.sumBy (fun x -> int (x ** length)) |> (=) number
